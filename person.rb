6bc603bd8aa66776ea07438d7fb064c477171886
Wael Elsafty <waelelsafty07@gmail.com>
2023-07-29 12:21:26 +0300

    Fix: linters

--- person.rb
+++ person.rb
@@ -3,7 +3,7 @@ require_relative 'nameable'
 class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id, :rentals
-  
+
   def initialize(age, name = 'unknown', parent_permission: true)
     super()
     @id = Random.rand(1..188)
@@ -12,12 +12,12 @@ class Person < Nameable
     @parent_permission = parent_permission
     @rentals = []
   end
-  
+
   def add_rental(rental)
     @rentals << rental
     rental.person = self
   end
-  
+
   def can_use_services?
     of_age? || @parent_permission
   end
--- student.rb
+++ student.rb
@@ -2,7 +2,7 @@ require_relative 'person'
 
 class Student < Person
   attr_accessor :classroom
-  
+
   def initialize(classroom, age, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
