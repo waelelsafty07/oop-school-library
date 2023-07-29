6c51c9a3a27c3ee52e44874d7c394f767143e095
Wael Elsafty <waelelsafty07@gmail.com>
2023-07-29 10:41:33 +0300

    decorate a class

--- person.rb
+++ person.rb
@@ -1,5 +1,7 @@
-class Person
+require_relative 'nameable'
+class Person < Nameable
   def initialize(age, name = 'unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..188)
     @name = name
     @age = age
